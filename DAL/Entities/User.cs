bc8e36734413152da509fe9a8a5754c5631e07b4
Nico Agote <agonico99@gmail.com>
2017-09-01 12:30:01 -0300

    Viernes me estoy yendo

--- DAL/Entities/User.cs
+++ DAL/Entities/User.cs
@@ -5,9 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 using DAL.Enums;
 using DAL.Interfaces;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace DAL.Entities
 {
+    [Table("Users")]
     public class User : IHeadData, ISoftDeleted
     {
         public User()
@@ -15,22 +18,40 @@ namespace DAL.Entities
             this.UserAudits = new List<UserAudit>();
         }
 
+        [Key]
         public Guid Id { get; set; }
-        public string Firstname { get; set; }
-        public string Surname { get; set; }
-        public string Denomination { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public Sex Sex { get; set; }
-        public DateTime BirthDate { get; set;}
-        public Boolean Available { get; set; }
-        public Guid? CountryId { get; set; }
-        public DateTime AddDate { get; set; }
-        public DateTime UpdateDate { get; set; }
-        public virtual ICollection<UserAudit> UserAudits { get; set; }
-        public DateTime? DeleteDate { get; set; }
 
+        [StringLength(50)]
+        public string Firstname { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        public string Denomination { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public DateTime BirthDate { get; set;}
+
+        public Boolean Available { get; set; }
+
+        public Guid? CountryId { get; set; }
+
+        public DateTime AddDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+
+        public virtual ICollection<UserAudit> UserAudits { get; set; }
+
+        public DateTime? DeleteDate { get; set; }
 
     }
 }
--- Prueba1/Controllers/UserController.cs
+++ Prueba1/Controllers/UserController.cs
@@ -14,13 +14,68 @@ namespace Prueba1.Controllers
         // GET: User
         public ActionResult Create()
         {
+   
             return View("Create");
         }
         
         [HttpPost]
-        public ActionResult Create(User posteado)
+        public ActionResult Create(User post)
         {
-            return View(posteado);
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var context = DbContextFactory.Create())
+                    {
+                        var userService = new UserService(context);
+
+                        var userGetResult = userService.Get();
+
+                        if (userGetResult.Result == Results.Error || userGetResult.Result == Results.Error)
+                        {
+                            throw new Exception(userGetResult.ResultErrorMessage);
+                        }
+
+                        var users = userGetResult.ResultData;
+
+                        var newUser = new User()
+                        {
+                            Firstname = post.Firstname,
+                            Surname = post.Surname,
+                            Denomination = post.Denomination,
+                            Username = post.Username,
+                            Email = post.Email,
+                            Password = post.Password,
+                            Sex = post.Sex,
+                            BirthDate = post.BirthDate,
+                            AddDate = DateTime.Now,
+                            UpdateDate = DateTime.Now
+                        };
+
+
+
+                        var userAddResult = userService.Add(newUser);
+
+                        if (userAddResult.Result == Results.Error || userAddResult.Result == Results.Error)
+                        {
+                            throw new Exception(userAddResult.ResultErrorMessage);
+                        }
+
+                        context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    ViewBag.error = "Los datos que mandaste no coinciden con el tipo User. Estuviste cerca.";
+                    return View("Create");
+                }
+            }
+            catch (Exception e)
+            {
+                //Manejo de la exception.
+            }
+
+            return View(post);
         }
     }
 }
